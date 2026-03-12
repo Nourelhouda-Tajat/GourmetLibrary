e49d09843421009faa758c6a2941a0dc3e5b5b4e
youcode safi <tajat.nourelhouda@gmail.com>
2026-03-12 23:01:25 +0000

    add seeders

---
+++ gourmetLibrary/database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin Gourmet',
+            'email' => 'admin@gourmet.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Nour',
+            'email' => 'nour@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'gourmand',
+        ]);
+    }
+}
